76830855b683242b0ca6cfe2dd772fa3a869693d
Karl Horky <karl.horky@gmail.com>
2021-03-01 10:53:48 +0100

    Allow self-signed SSL certificate

--- util/database.js
+++ util/database.js
@@ -14,7 +14,10 @@ function connectOneTimeToDatabase() {
   let sql;
 
   if (process.env.NODE_ENV === 'production') {
-    sql = postgres({ ssl: true });
+    // Heroku needs SSL connections but
+    // has an "unauthorized" certificate
+    // https://devcenter.heroku.com/changelog-items/852
+    sql = postgres({ ssl: { rejectUnauthorized: false } });
   } else {
     if (!globalThis.__postgresSqlClient) {
       globalThis.__postgresSqlClient = postgres();
